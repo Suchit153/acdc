20fce3448d8fb2cedb8b44f8d6301d934737dbb1
Suchit153 <gupta.suchit@gmail.com>
2023-02-04 21:33:02 +0100

    Section 5 complete

--- catkin_workspace/src/workshops/section_5/etsi_visualization/src/ETSIViz.cpp
+++ catkin_workspace/src/workshops/section_5/etsi_visualization/src/ETSIViz.cpp
@@ -84,11 +84,11 @@ void ETSIViz::CAMCallback(const definitions::v2x_CAM& msg){
       // ### START CODE HERE
       // fill with information from message
       // use helping comments from Wiki
-      obj.IdInternal = 0; // stationID               // Task
-      float lon = 0;      // longitude (x)           // Task
-      float lat = 0;      // latitude (y)            // Task
-      float v_x = 0;      // velocity in x direction // Task
-      float v_y = 0;      // velocity in y direction // Task
+      obj.IdInternal = msg.header_stationID; // stationID               // Task
+      float lon = msg.basic_container.referencePosition_longitude;      // longitude (x)           // Task
+      float lat = msg.basic_container.referencePosition_latitude;      // latitude (y)            // Task
+      float v_x = msg.high_freq_container.speed_speedValue * std::cos(msg.high_freq_container.heading_headingValue);      // velocity in x direction // Task
+      float v_y = msg.high_freq_container.speed_speedValue * std::sin(msg.high_freq_container.heading_headingValue);      // velocity in y direction // Task
       // ### END CODE HERE
 
     obj.fMean.resize((int)definitions::ctra_model::COUNT);
